570e9b453854e31a8790f58ea01fcca6b8af18cf
Pedro Arthur <pfernandesvasconcelos@gmail.com>
2016-09-25 19:41:31 -0300

    created a Job implementation which verifies and notify students based on books loan

---
+++ src/main/java/br/edu/ifpb/padroes/projeto/sisbibliotecanotifier/job/NotifyStudentsJob.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifpb.padroes.projeto.sisbibliotecanotifier.job;
+
+import br.edu.ifpb.padroes.projeto.sisbiblioteca.model.EmprestimoBo;
+import org.quartz.DisallowConcurrentExecution;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+/**
+ *
+ * @author kieckegard
+ */
+@DisallowConcurrentExecution
+public class NotifyStudentsJob implements Job {
+    
+    private final EmprestimoBo emprestimoBo;
+    
+    public NotifyStudentsJob() {
+        emprestimoBo = new EmprestimoBo();
+    }
+
+    @Override
+    public void execute(JobExecutionContext jec) throws JobExecutionException {
+        System.out.println("O Processo de verificação & notificação de alunos começou...");
+        emprestimoBo.notificaAlunos();
+    }
+    
+}
