23e2798c83155143c417cb98e635fe2e4c93d035
Pedro Arthur <pfernandesvasconcelos@gmail.com>
2016-09-25 19:54:47 -0300

    Created a RuntimeException called EmprestimoEmAndamentoException

---
+++ src/main/java/br/edu/ifpb/padroes/projeto/sisbiblioteca/exceptions/EmprestimoEmAndamentoException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifpb.padroes.projeto.sisbiblioteca.exceptions;
+
+/**
+ *
+ * @author kieckegard
+ */
+public class EmprestimoEmAndamentoException extends RuntimeException {
+    
+    public EmprestimoEmAndamentoException(String msg) {
+        super(msg);
+    }
+    
+}
