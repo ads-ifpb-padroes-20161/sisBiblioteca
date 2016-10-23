0ee84ff292c0ee66546bef18130c1808bcd5f92a
Pedro Arthur <pfernandesvasconcelos@gmail.com>
2016-10-23 21:50:00 -0200

    created BloqueioDao interface with method to insert, update state, list and verify and disblock students

---
+++ src/main/java/br/edu/ifpb/padroes/projeto/sisbiblioteca/dao/BloqueioDao.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifpb.padroes.projeto.sisbiblioteca.dao;
+
+import br.edu.ifpb.padroes.projeto.sisbiblioteca.entities.Bloqueio;
+import br.edu.ifpb.padroes.projeto.sisbiblioteca.enums.EstadoBloqueioEnum;
+import java.util.List;
+
+/**
+ *
+ * @author kieckegard
+ */
+public interface BloqueioDao {
+    
+    void inserir(Bloqueio bloqueio);
+    void alterarEstado(Integer bloqueioId, EstadoBloqueioEnum estado);
+    List<Bloqueio> lista();
+    void verificaEDesbloqueiaAlunos();
+}
