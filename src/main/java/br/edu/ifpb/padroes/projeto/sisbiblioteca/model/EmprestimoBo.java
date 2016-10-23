63d63f94f75bd7c47a97edd8df7221a34a193d04
Pedro Arthur <pfernandesvasconcelos@gmail.com>
2016-10-23 21:33:22 -0200

    listEmprestimos method is now retrieving a  sorted list, and refactored EmprestimoBo

--- src/main/java/br/edu/ifpb/padroes/projeto/sisbiblioteca/model/EmprestimoBo.java
+++ src/main/java/br/edu/ifpb/padroes/projeto/sisbiblioteca/model/EmprestimoBo.java
@@ -6,7 +6,7 @@
 package br.edu.ifpb.padroes.projeto.sisbiblioteca.model;
 
 import br.edu.ifpb.padroes.projeto.sisbiblioteca.dao.AlunoDao;
-import br.edu.ifpb.padroes.projeto.sisbiblioteca.dao.Dao;
+import br.edu.ifpb.padroes.projeto.sisbiblioteca.dao.BloqueioDao;
 import br.edu.ifpb.padroes.projeto.sisbiblioteca.dao.EmprestimoDao;
 import br.edu.ifpb.padroes.projeto.sisbiblioteca.dao.FactoryProvider;
 import br.edu.ifpb.padroes.projeto.sisbiblioteca.dao.LivroDao;
@@ -14,11 +14,14 @@ import br.edu.ifpb.padroes.projeto.sisbiblioteca.entities.Aluno;
 import br.edu.ifpb.padroes.projeto.sisbiblioteca.entities.Bloqueio;
 import br.edu.ifpb.padroes.projeto.sisbiblioteca.entities.Emprestimo;
 import br.edu.ifpb.padroes.projeto.sisbiblioteca.entities.Livro;
+import br.edu.ifpb.padroes.projeto.sisbiblioteca.enums.EstadoBloqueioEnum;
 import br.edu.ifpb.padroes.projeto.sisbiblioteca.enums.EstadoEmprestimoEnum;
 import br.edu.ifpb.padroes.projeto.sisbiblioteca.exceptions.AlunoInabilitadoException;
 import br.edu.ifpb.padroes.projeto.sisbiblioteca.exceptions.EmprestimoAtrasadoException;
+import br.edu.ifpb.padroes.projeto.sisbiblioteca.exceptions.EmprestimoJaFinalizadoException;
 import br.edu.ifpb.padroes.projeto.sisbiblioteca.exceptions.LivroIndisponivelException;
 import java.time.LocalDate;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -26,53 +29,74 @@ import java.util.List;
  * @author kieckegard
  */
 public class EmprestimoBo {
-    
+
     private final EmprestimoDao emprestimoDao;
     private final LivroDao livroDao;
     private final AlunoDao alunoDao;
-    private final Dao<Bloqueio, Integer> bloqueioDao;
-    
+    private final BloqueioDao bloqueioDao;
+
     public EmprestimoBo() {
         emprestimoDao = FactoryProvider.createFactory(FactoryProvider.jdbc).getEmprestimoDao();
         livroDao = FactoryProvider.createFactory(FactoryProvider.jdbc).getLivroDao();
         alunoDao = FactoryProvider.createFactory(FactoryProvider.jdbc).getAlunoDao();
         bloqueioDao = FactoryProvider.createFactory(FactoryProvider.jdbc).getBloqueioDao();
     }
-    
-    public void realizarEmprestimo(Aluno aluno, Livro livro, LocalDate startDate) throws LivroIndisponivelException, AlunoInabilitadoException {
-        Emprestimo emprestimo = new Emprestimo(aluno,livro,startDate,startDate.plusDays(10),EstadoEmprestimoEnum.CORRENTE);
+
+    public Emprestimo realizarEmprestimo(Aluno aluno, Livro livro, LocalDate startDate) throws LivroIndisponivelException, AlunoInabilitadoException {
+        
+        Emprestimo emprestimo = new Emprestimo(aluno, livro, startDate, startDate.plusDays(10), EstadoEmprestimoEnum.ANDAMENTO);
+        
         emprestimo.processarEmprestimo();
-        emprestimoDao.add(emprestimo);
-        livroDao.atualizarEstoque(livro);
-        alunoDao.atualizarEstadoAluno(aluno);
+        
+        newEmprestimoOnDao(emprestimo);
+        
+        return emprestimo;
     }
-    
-    public void finalizaEmprestimo(Emprestimo emprestimo) throws EmprestimoAtrasadoException {
+
+    public void finalizaEmprestimo(Emprestimo emprestimo) throws EmprestimoAtrasadoException, EmprestimoJaFinalizadoException {
         try {
             emprestimo.finalizarEmprestimo();
+            finalizeEmprestimoOnDao(emprestimo);
         }
         catch (EmprestimoAtrasadoException ex) {
             LocalDate now = LocalDate.now();
-            Bloqueio bloqueio = new Bloqueio(now, now.plusDays(3),emprestimo.getAluno());
+            Bloqueio bloqueio = new Bloqueio(now, now.plusDays(3), emprestimo.getAluno(), EstadoBloqueioEnum.ANDAMENTO);
             //insere o bloqueio!
-            bloqueioDao.add(bloqueio);
+            bloqueioDao.inserir(bloqueio);
+            finalizeEmprestimoOnDao(emprestimo);
             throw new EmprestimoAtrasadoException("O Devolução do empréstimo foi feita com sucesso"
                     + " mas a entrega do mesmo atrasou. O Aluno ficará banido de realizar empréstimos"
                     + " durante 3 dias! ");
-        } finally {
-            //update Empréstimo e Livro
-            emprestimoDao.finalizarEmprestimo(emprestimo);
-            livroDao.atualizarEstoque(emprestimo.getLivro());
-            alunoDao.atualizarEstadoAluno(emprestimo.getAluno());
         }
     }
     
-    public List<Emprestimo> listarEmprestimos() {
-        return emprestimoDao.list();
+    private void newEmprestimoOnDao(Emprestimo emprestimo) {
+        
+        emprestimoDao.add(emprestimo);
+        
+        updateAlunoAndLivro(emprestimo.getAluno(), emprestimo.getLivro());
+    }
+
+    private void finalizeEmprestimoOnDao(Emprestimo emprestimo) {
+        
+        emprestimoDao.finalizarEmprestimo(emprestimo);
+        
+        updateAlunoAndLivro(emprestimo.getAluno(), emprestimo.getLivro());
     }
     
-    public Emprestimo getEmprestimoById(Integer id) {
-        //retorna o empréstimo correspondente ao Id
-        return null;
+    private void updateAlunoAndLivro(Aluno aluno, Livro livro) {
+        
+        livroDao.atualizarEstoque(livro);
+        alunoDao.atualizarEstadoAluno(aluno);
+    }
+
+    public List<Emprestimo> listarEmprestimos() { 
+        List<Emprestimo> emprestimos = emprestimoDao.list();
+        Collections.sort(emprestimos);
+        return Collections.unmodifiableList(emprestimos);
+    }
+
+    public Emprestimo getEmprestimoById(Integer id) { 
+        return emprestimoDao.get(id);
     }
 }
